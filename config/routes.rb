a20d0425a901a984358d2f74f7ca0e787482263d
Andrew Po <sickphilosopher@gmail.com>
2013-07-11 03:06:02 +0400

    sth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 SharinatorWebUi::Application.routes.draw do
+  get "root/root"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
